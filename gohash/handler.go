6d9ed2a5b6b2d9b96fbee03ee6958081cb80cab7
randomnerd <9384129+randomnerd@users.noreply.github.com>
2019-08-21 02:15:41 +0300

    Update handler.go

--- gohash/handler.go
+++ gohash/handler.go
@@ -6,5 +6,5 @@ import (
 
 // Handle a serverless request
 func Handle(req []byte) string {
-	return fmt.Sprintf("Hello, Go. You said: %s", string(req))
+	return fmt.Sprintf("Hello, Go111. You said: %s", string(req))
 }
